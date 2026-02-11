3dccf55a149c92820d3d844c74544a12cc5009a8
Daniel Allen <dan@aeternagroup.net>
2026-02-11 23:48:17 +0000

    Update to schema version v4.59.0

--- variables.tf
+++ variables.tf
@@ -25,14 +25,14 @@ EOT
     name                       = string
     dynamic_throttling_enabled = optional(bool)
     rai_policy_name            = optional(string)
-    version_upgrade_option     = optional(string, "OnceNewDefaultVersionAvailable")
+    version_upgrade_option     = optional(string) # Default: "OnceNewDefaultVersionAvailable"
     model = object({
       format  = string
       name    = string
       version = optional(string)
     })
     sku = object({
-      capacity = optional(number, 1)
+      capacity = optional(number) # Default: 1
       family   = optional(string)
       name     = string
       size     = optional(string)
